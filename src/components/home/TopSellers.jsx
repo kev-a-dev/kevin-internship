b725e128705696932ffa5be08e5d59f638dae58c
kevdev <kevlays@gmail.com>
2023-04-14 21:31:42 -0700

    updated topsellers

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      const {data} = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers');
+      setData(data);
+      setLoading(false);
+    }
+    fetchData();
+  }, [loading])
+
+  console.log(data)
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +29,45 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to="/author">
+                        <div
+                          className="lazy pp-author skeleton avatar"
+                          src={''}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <div className="skeleton name">&nbsp;</div>
+                      <span className="skeleton price">&nbsp;</span>
+                    </div>
+                  </li>
+                ))
+              ) : (
+                data.map((nft, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${nft.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={nft.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${nft.authorId}`}>{nft.authorName}</Link>
+                      <span>{nft.price} ETH</span>
+                    </div>
+                  </li>
+                ))
+              )}
             </ol>
           </div>
         </div>
